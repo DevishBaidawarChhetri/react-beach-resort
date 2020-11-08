430565a6de2ae4c63205d3d50e0c05f7773a3cc2
Devish Baidawar Chhetri <devishbaidawar@gmail.com>
2020-11-08 09:48:41 +0545

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import Home from './pages/Home'
 import Rooms from './pages/Rooms'
 import SingleRoom from './pages/SingleRoom'
 import Error from './pages/Error'
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Navbar from './components/Navbar'
 function App () {
 	return (
 		<React.Fragment>
-			<h1>Welcome to Beach Resort.</h1>
-			<Home />
-			<Rooms />
-			<SingleRoom />
-			<Error />
-
+			<Router>
+				<Navbar />
+				<Switch>
+					<Route exact path="/" component={ Home } />
+					<Route exact path="/rooms/" component={ Rooms } />
+					<Route exact path="/rooms/:slug" component={ SingleRoom } />
+					<Route component={ Error } />
+				</Switch>
+			</Router>
 		</React.Fragment>
 	);
 }
