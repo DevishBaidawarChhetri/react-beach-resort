9a62d20145560f68e6ee1de03ef4cc120777c12c
Devish Baidawar Chhetri <devishbaidawar@gmail.com>
2020-11-09 09:49:23 +0545

    Added Services Section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,20 +2,24 @@ import React from 'react'
 import Hero from '../components/Hero'
 import Banner from '../components/Banner'
 import { Link } from 'react-router-dom'
+import Services from "../components/Services"
 
 
 function Home () {
 	return (
-		<Hero>
-			<Banner
-				title="Luxurious Rooms"
-				subtitle="Delux rooms starting at $299"
-			>
-				<Link to="/rooms" className="btn-primary">
-					Our Rooms
+		<React.Fragment>
+			<Hero>
+				<Banner
+					title="Luxurious Rooms"
+					subtitle="Delux rooms starting at $299"
+				>
+					<Link to="/rooms" className="btn-primary">
+						Our Rooms
 				</Link>
-			</Banner>
-		</Hero>
+				</Banner>
+			</Hero>
+			<Services />
+		</React.Fragment>
 	)
 }
 
