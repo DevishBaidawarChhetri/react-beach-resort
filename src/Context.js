cbfc2ad2dff8d85e3e77c8da516cca56773b81a8
Devish Baidawar Chhetri <devishbaidawar@gmail.com>
2020-11-17 20:10:34 +0545

    Added Guest filter

--- src/Context.js
+++ src/Context.js
@@ -3,119 +3,130 @@ import items from './data';
 const RoomContext = React.createContext();
 
 class RoomProvider extends Component {
-	state = {
-		rooms: [],
-		sortedRooms: [],
-		featuredRooms: [],
-		loading: true,
-		// For filtering the rooms
-		type: 'all',
-		capacity: 1,
-		price: 0,
-		minPrice: 0,
-		maxPrice: 0,
-		minSize: 0,
-		maxSize: 0,
-		breakfast: false,
-		pets: false
-	};
+  state = {
+    rooms: [],
+    sortedRooms: [],
+    featuredRooms: [],
+    loading: true,
+    // For filtering the rooms
+    type: 'all',
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false
+  };
 
-	componentDidMount () {
-		let rooms = this.formatData( items );
-		// console.log( rooms );
-		let featuredRooms = rooms.filter( room => room.featured === true );
-		let maxPrice = Math.max( rooms.map( item => item.price ) );
-		let maxSize = Math.max( rooms.map( item => item.size ) );
+  componentDidMount () {
+    let rooms = this.formatData( items );
+    // console.log( rooms );
+    let featuredRooms = rooms.filter( room => room.featured === true );
+    let maxPrice = Math.max( rooms.map( item => item.price ) );
+    let maxSize = Math.max( rooms.map( item => item.size ) );
 
-		this.setState( {
-			rooms,
-			sortedRooms: rooms,
-			featuredRooms,
-			loading: false,
-			price: maxPrice,
-			maxPrice,
-			maxSize
-		} );
-	}
+    this.setState( {
+      rooms,
+      sortedRooms: rooms,
+      featuredRooms,
+      loading: false,
+      price: maxPrice,
+      maxPrice,
+      maxSize
+    } );
+  }
 
-	formatData ( items ) {
-		let tempItems = items.map( item => {
-			let id = item.sys.id;
-			let images = item.fields.images.map( image => image.fields.file.url );
-			let room = { ...item.fields, images, id };
-			return room;
-		} );
-		return tempItems;
-	}
+  formatData ( items ) {
+    let tempItems = items.map( item => {
+      let id = item.sys.id;
+      let images = item.fields.images.map( image => image.fields.file.url );
+      let room = { ...item.fields, images, id };
+      return room;
+    } );
+    return tempItems;
+  }
 
-	getRoom = ( slug ) => {
-		let tempRooms = [ ...this.state.rooms ];
-		const room = tempRooms.find( ( room ) => room.slug === slug );
-		return room;
-	}
+  getRoom = ( slug ) => {
+    let tempRooms = [ ...this.state.rooms ];
+    const room = tempRooms.find( ( room ) => room.slug === slug );
+    return room;
+  }
 
-	handleChange = ( event ) => {
-		const target = event.target;
-		const value = event.type === 'checkbox' ? target.checked : target.value;
-		const name = event.target.name;
-		this.setState( {
-			[ name ]: value
-		}, this.filterRooms );
-	}
+  handleChange = ( event ) => {
+    const target = event.target;
+    const value = event.type === 'checkbox' ? target.checked : target.value;
+    const name = event.target.name;
+    this.setState( {
+      [ name ]: value
+    }, this.filterRooms );
+  }
 
-	filterRooms = () => {
-		// console.log( 'Hello World!' );
-		let {
-			rooms,
-			type,
-			capacity,
-			price,
-			minSize,
-			maxSize,
-			breakfast,
-			pets
-		} = this.state;
+  filterRooms = () => {
+    // console.log( 'Hello World!' );
+    let {
+      rooms,
+      type,
+      capacity,
+      price,
+      minSize,
+      maxSize,
+      breakfast,
+      pets
+    } = this.state;
 
-		let tempRooms = [ ...rooms ];
+    let tempRooms = [ ...rooms ];
 
-		if ( type !== 'all' )
-		{
-			tempRooms = tempRooms.filter( ( room ) => room.type === type );
-		}
-		// console.log( tempRooms );
-		this.setState( {
-			sortedRooms: tempRooms
-		} );
-	}
+    // Transforming value into int
+    capacity = parseInt( capacity );
 
-	render () {
-		return (
-			<RoomContext.Provider value={
-				{
-					...this.state,
-					getRoom: this.getRoom,
-					handleChange: this.handleChange
-				}
-			}>
-				{ this.props.children }
-			</RoomContext.Provider>
-		)
-	}
+    // Filter by type
+    if ( type !== 'all' )
+    {
+      tempRooms = tempRooms.filter( ( room ) => room.type === type );
+    }
+    // console.log( tempRooms );
+
+    // Filter by capacity
+    if ( capacity !== 1 )
+    {
+      tempRooms = tempRooms.filter( ( room ) => room.capacity >= capacity );
+    }
+
+    this.setState( {
+      sortedRooms: tempRooms
+    } );
+  }
+
+  render () {
+    return (
+      <RoomContext.Provider value={
+        {
+          ...this.state,
+          getRoom: this.getRoom,
+          handleChange: this.handleChange
+        }
+      }>
+        { this.props.children }
+      </RoomContext.Provider>
+    )
+  }
 }
 
 const RoomConsumer = RoomContext.Consumer;
 
 // Higher Order Component
 export function withRoomConsumer ( Component ) {
-	return function ConsumerWrapper ( props ) {
-		return (
-			<RoomConsumer>
-				{
-					value => <Component { ...props } context={ value } />
-				}
-			</RoomConsumer>
-		)
-	}
+  return function ConsumerWrapper ( props ) {
+    return (
+      <RoomConsumer>
+        {
+          value => <Component { ...props } context={ value } />
+        }
+      </RoomConsumer>
+    )
+  }
 }
 
 export { RoomProvider, RoomConsumer, RoomContext };
--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -1,50 +1,68 @@
 import React, { useContext } from 'react';
 import { RoomContext } from '../Context';
 import Title from '../components/Title';
-// Get Unique Values
-const getUnique = ( items, value ) => {
-	return [ ...new Set( items.map( item => item[ value ] ) ) ]
-}
+
 export default function RoomsFilter ( { rooms } ) {
-	const context = useContext( RoomContext );
-	// console.log( context );
-	const {
-		handleChange,
-		type,
-		capacity,
-		price,
-		minPrice,
-		maxPrice,
-		minSize,
-		maxSize,
-		breakfast,
-		pets
-	} = context;
+  const context = useContext( RoomContext );
+  // console.log( context );
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets
+  } = context;
 
-	// Get Unique Room Types
-	let types = getUnique( rooms, 'type' );
+  // Get Unique Values
+  const getUnique = ( items, value ) => {
+    return [ ...new Set( items.map( item => item[ value ] ) ) ]
+  }
 
-	// Add All in types
-	types = [ 'all', ...types ];
+  // Get Unique Room Types
+  let types = getUnique( rooms, 'type' );
 
-	// Maping to jsx
-	types = types.map( ( item, index ) => {
-		return <option value={ item } key={ index }>{ item }</option>
-	} );
+  // Add type "all" in types array
+  types = [ 'all', ...types ];
 
-	return (
-		<section className="filter-container">
-			<Title title="search rooms" />
-			<form className="filter-form">
-				{/* Select Type Begins */ }
-				<div className="form-group">
-					<label htmlFor="type">Room Type</label>
-					<select name="type" id="type" value={ type } className="form-control" onChange={ handleChange }>
-						{ types }
-					</select>
-				</div>
-				{/* Select Type Ends */ }
-			</form>
-		</section>
-	)
+  // Maping to jsx
+  types = types.map( ( item, index ) => {
+    return <option value={ item } key={ index }>{ item }</option>
+  } );
+
+  // Getting and Mapping Capacity
+  let peopleCapacity = getUnique( rooms, 'capacity' );
+  peopleCapacity = peopleCapacity.map( ( item, index ) => {
+    return <option key={ index } value={ item }>{ item }</option>
+  } )
+
+  return (
+    <section className="filter-container">
+      <Title title="search rooms" />
+      <form className="filter-form">
+        {/* Select Type Begins */ }
+        <div className="form-group">
+          <label htmlFor="type">Room Type</label>
+          <select name="type" id="type" value={ type } className="form-control" onChange={ handleChange }>
+            { types }
+          </select>
+        </div>
+        {/* Select Type Ends */ }
+
+        {/* Guest Begins */ }
+        <div className="form-group">
+          <label htmlFor="capacity">Guests</label>
+          <select name="capacity" id="capacity" value={ capacity } className="form-control" onChange={ handleChange }>
+            { peopleCapacity }
+          </select>
+        </div>
+        {/* Guest Ends */ }
+
+      </form>
+    </section>
+  )
 }
